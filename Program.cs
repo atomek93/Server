ec741b423a441b7f913c79538ef5453a871d3da6
atomek93 <oktny8@gmail.com>
2020-01-30 14:25:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ASPNetCoreWebAPI.Models;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (System.IO.File.Exists("data.json"))
+            {
+                String JSONtxt = System.IO.File.ReadAllText("data.json");
+                List<Person> people = JsonConvert.DeserializeObject<List<Person>>(JSONtxt);
+
+                Console.WriteLine("Előzőleg bevitt adatok:\n");
+                Console.WriteLine(JsonConvert.SerializeObject(people, Formatting.Indented));
+                Console.WriteLine();
+            }
+
+            WebHost
+                .CreateDefaultBuilder(args)
+                .ConfigureServices(services => services.AddMvc())
+                .Configure(app => app.UseMvc())
+                .UseUrls("http://*:80")
+                .Build()
+                .Run();
+        }
+    }
+}
