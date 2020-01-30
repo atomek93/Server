6c1f20cf6a7eb6cf07ee3d717428443c53aea530
atomek93 <oktny8@gmail.com>
2020-01-30 14:26:18 +0100

    Add files via upload

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using ASPNetCoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ASPNetCoreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("person")]
+    public class PersonController : ControllerBase
+    {
+        static public List<Person> people = new List<Person>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> Get()
+        {
+            if (System.IO.File.Exists("data.json"))
+            {
+                String JSONtxt = System.IO.File.ReadAllText("data.json");
+                people = JsonConvert.DeserializeObject<List<Person>>(JSONtxt);
+            }
+
+            return Ok(people);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Person> Get(int id)
+        {
+            if (System.IO.File.Exists("data.json"))
+            {
+                String JSONtxt = System.IO.File.ReadAllText("data.json");
+                people = JsonConvert.DeserializeObject<List<Person>>(JSONtxt);
+            }
+
+            if (id >= 0 && id < people.Count)
+            {
+                return Ok(people[id]);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Person person)
+        {
+            people.Add(person);
+
+            string json = JsonConvert.SerializeObject(people.ToArray());
+
+            System.IO.File.WriteAllText("data.json", json);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            people.RemoveAt(id);
+
+            string json = JsonConvert.SerializeObject(people.ToArray());
+
+            System.IO.File.WriteAllText("data.json", json);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ASPNetCoreWebAPI.Models
+{
+    public class Person
+    {
+        public string Nev { get; set; }
+        public string Lakcim { get; set; }
+        public string Tajszam { get; set; }
+        public string Panasz { get; set; }
+        public DateTime Idopont { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nev} {Tajszam} {Idopont:yyyy.MM.dd. hh:mm:ss}";
+        }
+    }
+}
